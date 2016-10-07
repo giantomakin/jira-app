2577cd57c6f831c9c0c5baff199b2d3fdb6f30ef
giantomakin <gian@marble.com>
2016-10-07 16:16:50 -0400

    clean code

--- public/javascripts/controllers/issues.controller.js
+++ public/javascripts/controllers/issues.controller.js
@@ -1,86 +1,86 @@
-angular.module('app.issues.controllers',['ngRoute']);
+angular.module('app.issues.controllers', ['ngRoute']);
 
 angular.module('app.issues.controllers')
 .controller('issuesCtrl', function($scope, $location, issuesService, $element) {
 
-	$scope.showloading = true;
+    $scope.showloading = true;
 
-	issuesService.getIssues.then(function(response){
+    issuesService.getIssues.then(function(response) {
 
-		$scope.attachment = [];
-		$scope.showloading = false;
+        $scope.attachment = [];
+        $scope.showloading = false;
 
-		response.forEach(function(issues, i){
+        response.forEach(function(issues, i) {
 
-			issuesService.getIssue(issues.id).then(function(issue){
+            issuesService.getIssue(issues.id).then(function(issue) {
 
-				if (typeof issue.fields.attachment[0] == 'object') {
-					issues.attachment = issue.fields.attachment[0].content;
-				} else {
-					issues.attachment = 'http://placehold.it/900x300';
-				}
+                if (typeof issue.fields.attachment[0] == 'object') {
+                    issues.attachment = issue.fields.attachment[0].content;
+                } else {
+                    issues.attachment = 'http://placehold.it/900x300';
+                }
 
-			});
+            });
 
-		});
+        });
 
 
-		$scope.issues = response;
+        $scope.issues = response;
 
-	});
+    });
 
 });
 
 angular.module('app.issues.controllers')
 .controller('issueCtrl', function($scope, $element, $location, issuesService) {
 
-	$scope.showloading = true;
+    $scope.showloading = true;
 
-	var $key = $element.find('#issue-key').val();
-	var progress = ['progress-bar-danger progress-bar-striped',
-					'progress-bar-warning progress-bar-striped active',
-					'progress-bar-info progress-bar-striped active',
-					'progress-bar-success progress-bar-striped active',
-					'progress-bar-success',
-					'progress-bar-danger progress-bar-striped active',
-					'progress-bar-success progress-bar',
-					];
+    var $key = $element.find('#issue-key').val();
+    var progress = ['progress-bar-danger progress-bar-striped',
+        'progress-bar-warning progress-bar-striped active',
+        'progress-bar-info progress-bar-striped active',
+        'progress-bar-success progress-bar-striped active',
+        'progress-bar-success',
+        'progress-bar-danger progress-bar-striped active',
+        'progress-bar-success progress-bar',
+    ];
 
-	function renderIssue(loader){
+    function renderIssue(loader) {
 
 
-		issuesService.getIssue($key).then(function(response){
+        issuesService.getIssue($key).then(function(response) {
 
-			var issue = response.fields;
-			var scenes = issue.customfield_10201;
-			var drawing = issue.customfield_10205 === null ? 'not-started' : issue.customfield_10205.value;
-			var doc = issue.customfield_10206 === null ? 'not-started' : issue.customfield_10206.value;
-			var doc_val = doc === 'done' ? 100 : 50;
-			var drawing_val = drawing === 'done' ? 100 : 50;
+            var issue = response.fields;
+            var scenes = issue.customfield_10201;
+            var drawing = issue.customfield_10205 === null ? 'not-started' : issue.customfield_10205.value;
+            var doc = issue.customfield_10206 === null ? 'not-started' : issue.customfield_10206.value;
+            var doc_val = doc === 'done' ? 100 : 50;
+            var drawing_val = drawing === 'done' ? 100 : 50;
 
-			issuesService.computeScene($scope, scenes, progress);
-			issuesService.computeDoc($scope, doc, progress);
-			issuesService.computeDrawing($scope, drawing, progress);
-			$scope.summary = issue.summary;
-			$scope.description = issue.description;
-			$scope.attachment = issue.attachment[0].content;
-			$scope.doc = doc;
-			$scope.drawing = drawing;
-			$scope.doc_value = doc_val;
-			$scope.drawing_value = drawing_val;
-			$scope.updated = 'updated: '+moment(issue.updated).fromNow();
-			issuesService.computeTotal($scope, progress);
+            issuesService.computeScene($scope, scenes, progress);
+            issuesService.computeDoc($scope, doc, progress);
+            issuesService.computeDrawing($scope, drawing, progress);
+            $scope.summary = issue.summary;
+            $scope.description = issue.description;
+            $scope.attachment = issue.attachment[0].content;
+            $scope.doc = doc;
+            $scope.drawing = drawing;
+            $scope.doc_value = doc_val;
+            $scope.drawing_value = drawing_val;
+            $scope.updated = 'updated: ' + moment(issue.updated).fromNow();
+            issuesService.computeTotal($scope, progress);
 
-			if(loader) $scope.showloading = false;
+            if (loader) $scope.showloading = false;
 
-		});
+        });
 
-}
+    }
 
-renderIssue(true);
-setInterval(function(){
-	renderIssue();
-}, 300000);
+    renderIssue(true);
+    setInterval(function() {
+        renderIssue();
+    }, 300000);
 
 
 });
--- public/javascripts/services/issues.service.js
+++ public/javascripts/services/issues.service.js
@@ -1,178 +1,179 @@
-angular.module('app.issues.services',[]);
+angular.module('app.issues.services', []);
 angular.module('app.issues.services')
-.service('issuesService', function($http){
+.service('issuesService', function($http) {
 
-	this.getIssues = $http({
-		method: 'GET',
-		url: '/jira/issues',
-		headers: {'Content-type': 'application/json'}
+    this.getIssues = $http({
+        method: 'GET',
+        url: '/jira/issues',
+        headers: {
+            'Content-type': 'application/json'
+        }
 
-	}).then(function successCallback(response) {
+    }).then(function successCallback(response) {
 
-		return response.data.issues;
+        return response.data.issues;
 
-	}, function errorCallback(response) {
+    }, function errorCallback(response) {
 
-		throw new Error(response.responseText);
+        throw new Error(response.responseText);
 
-	});
+    });
 
-	this.getIssue = function(key){
+    this.getIssue = function(key) {
 
-		return $http({
-			method: 'GET',
-			url: '/jira/issue/'+key,
-			headers: {'Content-type': 'application/json'}
+        return $http({
+            method: 'GET',
+            url: '/jira/issue/' + key,
+            headers: {
+                'Content-type': 'application/json'
+            }
 
-		}).then(function successCallback(response) {
+        }).then(function successCallback(response) {
 
-			return response.data;
+            return response.data;
 
-		}, function errorCallback(response) {
+        }, function errorCallback(response) {
 
-			throw new Error(response.responseText);
+            throw new Error(response.responseText);
 
-		});
-	};
+        });
+    };
 
-	this.computeScene = function(scope, scenes, progress){
+    this.computeScene = function(scope, scenes, progress) {
 
-		var scenesDeferred = $.Deferred();
+        var scenesDeferred = $.Deferred();
 
-		if (scenes === null) {
+        if (scenes === null) {
 
-			console.log('no scenes');
+            console.log('no scenes');
 
-		} else {
+        } else {
 
-			var scenesArr = scenes.split(',');
-			var sceneObj = {};
-			var sceneVal = 0;
+            var scenesArr = scenes.split(',');
+            var sceneObj = {};
+            var sceneVal = 0;
 
-			var computeScene = scenesDeferred.pipe(function(size, total_value){
-				var _overall = size * 100;
-				var _partial = total_value / _overall;
-				var _total = _partial * 100;
+            var computeScene = scenesDeferred.pipe(function(size, total_value) {
+                var _overall = size * 100;
+                var _partial = total_value / _overall;
+                var _total = _partial * 100;
 
-				return _total == isNaN ? 0 : Math.round(_total) ;
-			});
+                return _total == isNaN ? 0 : Math.round(_total);
+            });
 
-			for(var scene in scenesArr){
-				var _scene = scenesArr[scene].split(':');
-				var _key = _scene[0];
-				var _val = _scene[1];
+            for (var scene in scenesArr) {
+                var _scene = scenesArr[scene].split(':');
+                var _key = _scene[0];
+                var _val = _scene[1];
 
-				sceneVal += parseInt(_val);
-				sceneObj[_key] = _val;
-			}
+                sceneVal += parseInt(_val);
+                sceneObj[_key] = _val;
+            }
 
 
-			scenesDeferred.resolve(Object.keys(sceneObj).length,sceneVal);
+            scenesDeferred.resolve(Object.keys(sceneObj).length, sceneVal);
 
-			computeScene.done(function(value){
+            computeScene.done(function(value) {
 
-				scope.total = value;
+                scope.total = value;
 
-				if ( value < 25 ) scope.scene_progress = progress[0];
+                if (value < 25) scope.scene_progress = progress[0];
 
-				if ( value >= 25 && value <= 50 ) scope.scene_progress = progress[1];
+                if (value >= 25 && value <= 50) scope.scene_progress = progress[1];
 
-				if ( value >= 50 && value <= 75 ) scope.scene_progress = progress[2];
+                if (value >= 50 && value <= 75) scope.scene_progress = progress[2];
 
-				if ( value >= 75 && value <= 99 ) scope.scene_progress = progress[3];
+                if (value >= 75 && value <= 99) scope.scene_progress = progress[3];
 
-				if ( value == 100 ) scope.scene_progress = progress[4];
+                if (value == 100) scope.scene_progress = progress[4];
 
-			});
-		}
+            });
+        }
 
+    };
 
+    this.computeDoc = function($scope, doc, progress) {
 
+        if (doc === null) {
 
-	};
+            console.log('no docs');
 
-	this.computeDoc = function($scope, doc, progress){
+        } else {
 
-		if ( doc === null ) {
+            if (doc == 'not-started') {
+                $scope.doc_progress = progress[5];
+            } else if (doc == 'in-progress') {
+                $scope.doc_progress = progress[1];
+            } else if (doc == 'done') {
+                $scope.doc_progress = progress[6];
+            }
 
-			console.log('no docs');
+        }
 
-		} else {
 
-			if (doc == 'not-started') {
-				$scope.doc_progress = progress[5];
-			} else if (doc == 'in-progress') {
-				$scope.doc_progress = progress[1];
-			} else if (doc == 'done') {
-				$scope.doc_progress = progress[6];
-			}
+    };
 
-		}
+    this.computeDrawing = function($scope, drawing, progress) {
 
 
-	};
+        if (drawing === null) {
 
-	this.computeDrawing = function($scope, drawing, progress){
+            console.log('no drawing');
 
+        } else {
 
-		if ( drawing === null ) {
+            if (drawing == 'not-started') {
+                $scope.drawing_progress = progress[5];
+            } else if (drawing == 'in-progress') {
+                $scope.drawing_progress = progress[1];
+            } else if (drawing == 'done') {
+                $scope.drawing_progress = progress[6];
+            }
 
-			console.log('no drawing');
+        }
 
-		} else {
 
-			if (drawing == 'not-started') {
-				$scope.drawing_progress = progress[5];
-			} else if (drawing == 'in-progress') {
-				$scope.drawing_progress = progress[1];
-			} else if (drawing == 'done') {
-				$scope.drawing_progress = progress[6];
-			}
+    };
 
-		}
+    this.addUp = function(addup) {
 
+        var result = addup;
 
-	};
+        return result;
+    };
 
-	this.addUp = function(addup){
+    this.computeTotal = function($scope, progress) {
 
-		var result = addup;
+        var totalDeferred = $.Deferred();
+        var addup = $scope.doc_value + $scope.drawing_value + $scope.total;
+        var computeTotal = totalDeferred.pipe(function(t) {
+            var _total_value = t;
+            var _partial = _total_value / 300;
+            var _total = _partial * 100;
 
-		return result;
-	};
+            return _total == isNaN ? 0 : Math.round(_total);
+        });
 
-	this.computeTotal = function($scope, progress){
 
-		var totalDeferred = $.Deferred();
-		var addup = $scope.doc_value + $scope.drawing_value + $scope.total;
-		var computeTotal = totalDeferred.pipe(function(t){
-			var _total_value = t;
-			var _partial = _total_value / 300;
-			var _total = _partial * 100;
+        totalDeferred.resolve(this.addUp(addup));
 
-			return _total == isNaN ? 0 : Math.round(_total);
-		});
+        computeTotal.done(function(value) {
 
+            $scope.total_progress = value;
 
-		totalDeferred.resolve(this.addUp(addup));
+            if (value < 25) $scope.total_progressbar = progress[0];
 
-		computeTotal.done(function(value){
+            if (value >= 25 && value <= 50) $scope.total_progressbar = progress[1];
 
-			$scope.total_progress = value;
+            if (value >= 50 && value <= 75) $scope.total_progressbar = progress[2];
 
-			if ( value < 25 ) $scope.total_progressbar = progress[0];
+            if (value >= 75 && value <= 99) $scope.total_progressbar = progress[3];
 
-			if ( value >= 25 && value <= 50 ) $scope.total_progressbar = progress[1];
+            if (value == 100) $scope.total_progressbar = progress[4];
 
-			if ( value >= 50 && value <= 75 ) $scope.total_progressbar = progress[2];
+        });
 
-			if ( value >= 75 && value <= 99 ) $scope.total_progressbar = progress[3];
-
-			if ( value == 100 ) $scope.total_progressbar = progress[4];
-
-		});
-
-	};
+    };
 
 
 });
